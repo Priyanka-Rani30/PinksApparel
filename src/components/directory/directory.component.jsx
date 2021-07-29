1ee7b5015915e3324e9448e64faaa256e748bdb6
Priyanka Rani <priyanka.rani@ulta.com>
2021-07-29 16:40:33 +0530

    update menu-items component with withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ class Directory extends React.Component {
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({ id, imageUrl, title, size }) => (
-                    <MenuItems key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItems key={id} {...otherSectionProps} />
                 ))}
             </div>
         )
--- src/components/menu-items/menu-items.component.jsx
+++ src/components/menu-items/menu-items.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './menu-items.styles.scss';
+import { withRouter } from 'react-router';
 
-const MenuItems = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`} >
-        <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+const MenuItems = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`} onClick={() => {
+        history.push(`${match.url}${linkUrl}`)
+    }}>
+
+        <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}
+        />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
@@ -11,4 +16,4 @@ const MenuItems = ({ title, imageUrl, size }) => (
     </div >
 )
 
-export default MenuItems;
+export default withRouter(MenuItems);
